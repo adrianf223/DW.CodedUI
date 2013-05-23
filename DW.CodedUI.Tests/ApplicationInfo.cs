2a8d332a036bd0f81d397cad7ff1d9bce2a57299
David W <david.w.dev@gmail.com>
2013-05-23 20:18:14 +0200

    Adjust path to the test application to an absolute path. On VS 2012 the relative makes problems for the Process to start

--- DW.CodedUI.Tests/ApplicationInfo.cs
+++ DW.CodedUI.Tests/ApplicationInfo.cs
@@ -1,8 +1,19 @@
-﻿namespace DW.CodedUI.Tests
+﻿using System;
+using System.IO;
+
+namespace DW.CodedUI.Tests
 {
     internal static class ApplicationInfo
     {
-        internal const string ExecutablePath = @"..\..\..\DW.CodedUI.Demo\bin\Debug\DW.CodedUI.Demo.exe";
+        internal static string ExecutablePath
+        {
+            get
+            {
+                var currentDirectory = Environment.CurrentDirectory;
+                var path = Path.Combine(currentDirectory, @"..\..\..\DW.CodedUI.Demo\bin\Debug\2012\DW.CodedUI.Demo.exe");
+                return path;
+            }
+        }
         internal const string FastStartArguments = "/FastStart";
         internal const string WindowTitle = "Application Window Title";
         internal const int StartupWaitTime = 3000;
