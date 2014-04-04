cdab8eccd5276adae01bf594a668ecd665950802
David W <david.w.dev@gmail.com>
2014-04-04 23:38:08 +0200

    Add missing file

---
+++ DW.CodedUI.Tryouts/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DW.CodedUI.Tryouts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("DW.CodedUI.Tryouts")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("cf32be38-999a-4aee-b660-94bcd84d2ddc")]
