2723461df0fc80a4646f69c02e957d6116a1a8da
David W <david.w.dev@gmail.com>
2014-04-04 20:41:59 +0200

    Add documentations part VI

--- DW.CodedUI/MouseEx.cs
+++ DW.CodedUI/MouseEx.cs
@@ -7,73 +7,154 @@ using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
 
 namespace DW.CodedUI
 {
+    /// <summary>
+    /// Provides static methods for performing mouse actions in a user interface.
+    /// </summary>
     public static class MouseEx
     {
+        /// <summary>
+        /// Executes a mouse click.
+        /// </summary>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click()
         {
             Mouse.Click();
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse click while holding the modifier keys.
+        /// </summary>
+        /// <param name="modifierKeys">The modifier keys to be hold while the click is executed.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(ModifierKeys modifierKeys)
         {
             Mouse.Click(modifierKeys);
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse click with a specific mouse button.
+        /// </summary>
+        /// <param name="button">The mouse button that will be used for clicking.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(MouseButtons button)
         {
             Mouse.Click(button);
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse click at a specific position within the screen.
+        /// </summary>
+        /// <param name="screenCoordinate">The point on the whole screen where to click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(Point screenCoordinate)
         {
             Mouse.Click(screenCoordinate);
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse click at a specific position within the screen by using a specific mouse button and holding a modifier key.
+        /// </summary>
+        /// <param name="button">The mouse button that will be used for clicking.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the click is executed.</param>
+        /// <param name="screenCoordinate">The point on the whole screen where to click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(MouseButtons button, ModifierKeys modifierKeys, Point screenCoordinate)
         {
             Mouse.Click(button, modifierKeys, screenCoordinate);
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse click in the center of the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(BasicElement element)
         {
             return Click(element, MouseButtons.Left, ModifierKeys.None, null);
         }
 
+        /// <summary>
+        /// Executes a mouse click on a relative position inside the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <param name="relativePosition">The relative position in the control where to click.</param>
+        /// <returns>A combinable Do to be able to append additional actions</returns>
         public static CombinableDo Click(BasicElement element, At relativePosition)
         {
             return Click(element, MouseButtons.Left, ModifierKeys.None, relativePosition);
         }
 
+        /// <summary>
+        /// Executes a mouse click in the center of the given basic element with holding modifier keys.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the click is executed.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(BasicElement element, ModifierKeys modifierKeys)
         {
             return Click(element, MouseButtons.Left, modifierKeys, null);
         }
 
+        /// <summary>
+        /// Executes a mouse click at a relative position of the given basic element with holding modifier keys.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the click is executed.</param>
+        /// <param name="relativePosition">The relative position in the control where to click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(BasicElement element, ModifierKeys modifierKeys, At relativePosition)
         {
             return Click(element, MouseButtons.Left, modifierKeys, relativePosition);
         }
 
+        /// <summary>
+        /// Executes a mouse click with a specific mouse buttons in the center of the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <param name="button">The mouse button that will be used for clicking.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(BasicElement element, MouseButtons button)
         {
             return Click(element, button, ModifierKeys.None, null);
         }
 
+        /// <summary>
+        /// Executes a mouse click with a specific mouse buttons on a relative position in the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <param name="button">The mouse button that will be used for clicking.</param>
+        /// <param name="relativePosition">The relative position in the control where to click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(BasicElement element, MouseButtons button, At relativePosition)
         {
             return Click(element, button, ModifierKeys.None, relativePosition);
         }
 
+        /// <summary>
+        /// Executes a mouse click with a specific mouse buttons and hold modifier keys in the center of the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <param name="button">The mouse button that will be used for clicking.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the click is executed.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(BasicElement element, MouseButtons button, ModifierKeys modifierKeys)
         {
             return Click(element, button, modifierKeys, null);
         }
 
+        /// <summary>
+        /// Executes a mouse click with a specific mouse button and hold modifier keys in the relative position of the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <param name="button">The mouse button that will be used for clicking.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the click is executed.</param>
+        /// <param name="relativePosition">The relative position in the control where to click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo Click(BasicElement element, MouseButtons button, ModifierKeys modifierKeys, At relativePosition)
         {
             var rect = element.Properties.BoundingRectangle;
@@ -88,71 +169,149 @@ namespace DW.CodedUI
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse double click.
+        /// </summary>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick()
         {
             Mouse.DoubleClick();
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse double click while holding the modifier keys.
+        /// </summary>
+        /// <param name="modifierKeys">The modifier keys to be hold while the double click is executed.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(ModifierKeys modifierKeys)
         {
             Mouse.DoubleClick(modifierKeys);
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse double click with a specific mouse button.
+        /// </summary>
+        /// <param name="button">The mouse button that will be used for double clicking.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(MouseButtons button)
         {
             Mouse.DoubleClick(button);
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse double click at a specific position within the screen.
+        /// </summary>
+        /// <param name="screenCoordinate">The point on the whole screen where to double click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(Point screenCoordinate)
         {
             Mouse.DoubleClick(screenCoordinate);
             return new CombinableDo();
         }
 
-        public static CombinableDo DoubleClick(MouseButtons button, ModifierKeys modifierKeys, Point screenCoordinates)
+        /// <summary>
+        /// Executes a mouse double click at a specific position within the screen by using a specific mouse button and holding a modifier key.
+        /// </summary>
+        /// <param name="button">The mouse button that will be used for double clicking.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the double click is executed.</param>
+        /// <param name="screenCoordinate">The point on the whole screen where to double click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
+        public static CombinableDo DoubleClick(MouseButtons button, ModifierKeys modifierKeys, Point screenCoordinate)
         {
-            Mouse.DoubleClick(button, modifierKeys, screenCoordinates);
+            Mouse.DoubleClick(button, modifierKeys, screenCoordinate);
             return new CombinableDo();
         }
 
+        /// <summary>
+        /// Executes a mouse double click in the center of the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be double clicked.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(BasicElement element)
         {
             return DoubleClick(element, MouseButtons.Left, ModifierKeys.None, null);
         }
 
+        /// <summary>
+        /// Executes a mouse double click on a relative position inside the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be double clicked.</param>
+        /// <param name="relativePosition">The relative position in the control where to double click.</param>
+        /// <returns>A combinable Do to be able to append additional actions</returns>
         public static CombinableDo DoubleClick(BasicElement element, At relativePosition)
         {
             return DoubleClick(element, MouseButtons.Left, ModifierKeys.None, relativePosition);
         }
 
+        /// <summary>
+        /// Executes a mouse double click in the center of the given basic element with holding modifier keys.
+        /// </summary>
+        /// <param name="element">The basic element to be double clicked.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the double click is executed.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(BasicElement element, ModifierKeys modifierKeys)
         {
             return DoubleClick(element, MouseButtons.Left, modifierKeys, null);
         }
 
+        /// <summary>
+        /// Executes a mouse double click at a relative position of the given basic element with holding modifier keys.
+        /// </summary>
+        /// <param name="element">The basic element to be double clicked.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the double click is executed.</param>
+        /// <param name="relativePosition">The relative position in the control where to double click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(BasicElement element, ModifierKeys modifierKeys, At relativePosition)
         {
             return DoubleClick(element, MouseButtons.Left, modifierKeys, relativePosition);
         }
 
+        /// <summary>
+        /// Executes a mouse double click with a specific mouse buttons in the center of the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be double clicked.</param>
+        /// <param name="button">The mouse button that will be used for double clicking.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(BasicElement element, MouseButtons button)
         {
             return DoubleClick(element, button, ModifierKeys.None, null);
         }
 
+        /// <summary>
+        /// Executes a mouse double click with a specific mouse buttons on a relative position in the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be double clicked.</param>
+        /// <param name="button">The mouse button that will be used for double clicking.</param>
+        /// <param name="relativePosition">The relative position in the control where to double click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(BasicElement element, MouseButtons button, At relativePosition)
         {
             return DoubleClick(element, button, ModifierKeys.None, relativePosition);
         }
 
+        /// <summary>
+        /// Executes a mouse double click with a specific mouse buttons and hold modifier keys in the center of the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be double clicked.</param>
+        /// <param name="button">The mouse button that will be used for double clicking.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the double click is executed.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(BasicElement element, MouseButtons button, ModifierKeys modifierKeys)
         {
             return DoubleClick(element, button, modifierKeys, null);
         }
 
+        /// <summary>
+        /// Executes a mouse double click with a specific mouse button and hold modifier keys in the relative position of the given basic element.
+        /// </summary>
+        /// <param name="element">The basic element to be clicked.</param>
+        /// <param name="button">The mouse button that will be used for double clicking.</param>
+        /// <param name="modifierKeys">The modifier keys to be hold while the double click is executed.</param>
+        /// <param name="relativePosition">The relative position in the control where to double click.</param>
+        /// <returns>A combinable Do to be able to append additional actions.</returns>
         public static CombinableDo DoubleClick(BasicElement element, MouseButtons button, ModifierKeys modifierKeys, At relativePosition)
         {
             var rect = element.Properties.BoundingRectangle;
