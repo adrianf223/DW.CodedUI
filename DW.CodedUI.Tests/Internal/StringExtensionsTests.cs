a3ee2dbe7ab6f3a18c634a32bb8bccbc126357f7
David W <david.w.dev@gmail.com>
2014-04-06 18:10:17 +0200

    Sign test project assembly to be able to use InternalsVisibleTo; Add first tests

--- DW.CodedUI.Tests/Internal/StringExtensionsTests.cs
+++ DW.CodedUI.Tests/Internal/StringExtensionsTests.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DW.CodedUI.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DW.CodedUI.Tests.Internal
 {
     [TestClass]
     public class StringExtensionsTests
     {
+        [TestMethod]
+        public void Match_SourceStringIsNull_ReturnsFalse()
+        {
+            var matches = StringExtensions.Match(null, "asd", CompareKind.Contains);
+
+            Assert.IsFalse(matches);
+        }
+
+        [TestMethod]
+        public void Match_SearchPatternIsNull_ReturnsFalse()
+        {
+            var matches = StringExtensions.Match("asd", null, CompareKind.Contains);
+
+            Assert.IsFalse(matches);
+        }
     }
 }
--- DW.CodedUI/Properties/AssemblyInfo.cs
+++ DW.CodedUI/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DW.CodedUI - Test your UI fast and easy")]
@@ -8,3 +9,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2fd82625-aa9a-4bf6-b260-60afb89647e7")]
+
+[assembly: InternalsVisibleTo("DW.CodedUI.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c1564c54379baba821f4e6893a2832b1786614424ffc827c8f8bb1bda77c850369908ba353e4f7589d41019706510ddf2a541ae73eeb9c085d649bcd3887ae9ed24288b6563660409a4fb624a9566e62e4074b4a1f59290a8781e17ddc89f46075a2ba476c98c6c2c0371e32152599e3544d9fb6d0788ffb5dbb949db685d2b5")]
+
+/*
+ * For InternalsVisibleto
+ * - Sign the test project with the same key
+ * - Extract public key from it (sn -p my-libraries.pfx my-libraries.pub)
+ * - Show public key (sn -tp my-libraries.pub
+ * - Add the key on one line into the PublicKey= of the InternalsVisibleTo (See above)
+ * */
