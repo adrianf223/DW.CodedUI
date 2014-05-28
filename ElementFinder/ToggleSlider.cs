9a0b5a3258a0c9905a28891d6336c01a7654ecd3
David W <david.w.dev@gmail.com>
2014-05-28 20:00:55 +0200

    Use a toggle slider for a better visualization of the enabled and disabled state of the ElementFinder

---
+++ ElementFinder/ToggleSlider.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace ElementFinder
+{
+    public class ToggleSlider : ToggleButton
+    {
+        static ToggleSlider()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleSlider), new FrameworkPropertyMetadata(typeof(ToggleSlider)));
+        }
+    }
+}
