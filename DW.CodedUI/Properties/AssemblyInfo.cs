b934f59624d06ced9a749c7d4add47e713b08f15
David W <david.w.dev@gmail.com>
2014-07-12 15:34:47 +0200

    Add MIT license header

--- DW.CodedUI/Properties/AssemblyInfo.cs
+++ DW.CodedUI/Properties/AssemblyInfo.cs
@@ -1,4 +1,30 @@
-﻿using System.Reflection;
+﻿#region License
+/*
+The MIT License (MIT)
+
+Copyright (c) 2012-2014 David Wendland
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE
+*/
+#endregion License
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -11,11 +37,3 @@ using System.Runtime.InteropServices;
 [assembly: Guid("2fd82625-aa9a-4bf6-b260-60afb89647e7")]
 
 [assembly: InternalsVisibleTo("DW.CodedUI.Tests")]
-
-/*
- * For InternalsVisibleto
- * - Sign the test project with the same key
- * - Extract public key from it (sn -p my-libraries.pfx my-libraries.pub)
- * - Show public key (sn -tp my-libraries.pub
- * - Add the key on one line into the PublicKey= of the InternalsVisibleTo (See above)
- * */
