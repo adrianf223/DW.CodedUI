18fb83552e4cbfc948a64e21eac93e7ac017882c
David W <david.w.dev@gmail.com>
2014-07-15 22:21:36 +0200

    Release 2.2.0.2013

--- DW.CodedUI/Properties/CommonMeta.cs
+++ DW.CodedUI/Properties/CommonMeta.cs
@@ -29,5 +29,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("my-libraries David Wendland")]
 [assembly: AssemblyCopyright("Copyright © my-libraries David Wendland 2012-2014")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("2.1.0.2013")]
-[assembly: AssemblyFileVersion("2.1.0.2013")]
+[assembly: AssemblyVersion("2.2.0.2013")]
+[assembly: AssemblyFileVersion("2.2.0.2013")]
