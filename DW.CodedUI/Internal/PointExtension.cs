be5f4fc73c2845c5f496aa25240b4044a4ef1448
David Wendland <david.w.dev@gmail.com>
2018-04-15 10:29:37 +0200

    Forgot to add some files

---
+++ DW.CodedUI/Internal/PointExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace DW.CodedUI.Tests.Internal
+{
+    internal static class PointExtension
+    {
+        internal static System.Drawing.Point ToDrawingPoint(this System.Windows.Point point)
+        {
+            return new System.Drawing.Point((int)point.X, (int)point.Y);
+        }
+
+        internal static System.Windows.Point ToWindowsPoint(this System.Drawing.Point point)
+        {
+            return new System.Windows.Point(point.X, point.Y);
+        }
+    }
+}
